87e7ff201f4cd6a698f57a37ee84b0af5f68c8c2
Sureshsingh7 <53520905+Sureshsingh7@users.noreply.github.com>
2022-08-31 17:54:59 +0530

    Create Snowflake_Deployment.ps1

---
+++ ODS_build/Snowflake_Deployment.ps1
@@ -0,0 +1,12 @@
+param(
+[String] $configFile,
+[String] $basePath,
+[String] $env,
+[String] $outputPath,
+[String] $accountName
+)
+Write-Host "configfile path is $configFile"
+Write-Host "replace dir with $basePath"
+Write-Host "environment is $env"
+Write-Host "environment is $outputPath"
+Write-Host "environment is $accountName"
