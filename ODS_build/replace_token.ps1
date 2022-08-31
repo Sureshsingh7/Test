46c5be996e4ee18b27a2aa1551306502fa5adc68
Sureshsingh7 <53520905+Sureshsingh7@users.noreply.github.com>
2022-08-31 15:04:25 +0530

    Add files via upload

---
+++ ODS_build/replace_token.ps1
@@ -0,0 +1,9 @@
+﻿param(
+[String] $configFile,
+[String] $replaceDirectory,
+[string] $env
+)
+Write-Host "configfile path is $configFile"
+Write-Host "replace dir with $replaceDirectory"
+Write-Host "environment is $env"
+Out-File -FilePath D:\logs.txt
