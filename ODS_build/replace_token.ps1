5d074a887213a09378a0ab09bc6cb78f2299af5b
Sureshsingh7 <53520905+Sureshsingh7@users.noreply.github.com>
2022-09-01 17:01:04 +0530

    Update replace_token.ps1

--- ODS_build/replace_token.ps1
+++ ODS_build/replace_token.ps1
@@ -6,5 +6,5 @@
 Write-Host "configfile path is $configFile"
 Write-Host "replace dir with $replaceDirectory"
 Write-Host "environment is $env"
-New-Item -Path 'D:\$env' -ItemType Directory
-Set-Content D:\$env ' $configFile \n $replaceDirectory \n $env'
+New-Item -Path D:\$env -ItemType Directory
+Set-Content D:\$env $configFile \n $replaceDirectory \n $env
